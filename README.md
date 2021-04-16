0c399d6170ce213abf1eec4fb9bd2caf31fa07fc
fedpet <federico.pettinari2@studio.unibo.it>
2021-04-16 19:01:53 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# aws-sp
+# Getting started
+`docker compose up` then navigate to `localhost:8080`
--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,15 @@ const mongoose = require('mongoose');
 const app = express()
 app.use(express.json())
 
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser:true, useUnifiedTopology:true})
+const PORT = process.env.APP_PORT || 8080
+
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser:true, useUnifiedTopology:true})
 mongoose.set('returnOriginal', false) // update queries will return the modified object instead of the original one
 
 app.get('/', function(req, res) {
   res.send('Hello World')
 })
 
-app.listen(8080, function() {
-  console.log('listening on http://localhost:8080')
+app.listen(PORT, function() {
+  console.log('listening on http://localhost:' + PORT)
 })
