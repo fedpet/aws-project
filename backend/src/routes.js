5ec49b074b444b619c3414926742a9930bc45aae
Hamado Dene <hamado.dene@studio.unibo.it>
2021-05-22 19:29:55 +0200

    Fix

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,23 +5,18 @@ const { CastError } = require('mongoose')
 
 const account = require('./controllers/login')(createToken)
 const notification = require('./controllers/notificationController');
-
-
-router.use(auth.unless({ path: ['/login'] }))
-
 const waste = require('./controllers/waste')
 
-router.use( auth.unless({ path: ['/login', '/waste'] }))
-
-const notification = require('../controllers/notificationController');
-
-router.use(auth.unless({ path: ['/login'] }))
-
+router.use( auth.unless({ path: ['/login', '/waste'] }));
 
 router.post('/login', account.login)
 router.get('/account', account.account)
 router.post('/waste', waste.delivery)
-
+router.get('/account/:account/notifications', notification.getNotifications)
+router.get('/account/:account/notifications/:start_data/:end_data')
+router.route('/account/:account/notifications/:notification_id')
+    .get(notification.getNotificationById)
+    .post(notification.markAsRead)
 
 router.use((err, req, res, next) => {
     if(err instanceof CastError) {
@@ -30,17 +25,6 @@ router.use((err, req, res, next) => {
         next(err)
     }
 })
-
-//Notifications system
-router.route('/account/:account/notifications')
-    .get(notification.getNotifications)
-
-router.route('/account/:account/notifications/:notification_id')
-    .get(notification.getNotificationById)
-    .post(notification.markAsRead);
-
-router.get('/account/:account/notifications/:start_data/:end_data');
-
 
 // handle all 404 requests
 router.use(function(req, res) {
