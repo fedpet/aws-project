ca30a70046313f890aa66106d0cd06c121109041
Hamado Dene <hamado.dene@studio.unibo.it>
2021-05-26 21:45:36 +0200

    Simple handling if no token returns

--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -19,6 +19,7 @@ async function loginUser(credentials) {
 export default function Login({setToken}) {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+    const [errorMessage, setErrorMessage] = useState();
 
     const handleSubmit = async e => {
         e.preventDefault();
@@ -26,26 +27,33 @@ export default function Login({setToken}) {
             email,
             password
         });
-        setToken(token);
+        if(!token) {
+          setToken(token);
+        } else {
+            setErrorMessage('Authentication error. Incorrect name or password.')
+        }
     }
 
   return(
     <div className="container-fluid">
+
       <div className="row">
         <div className="col-sm-6 login-section-wrapper">
           <div className="brand-wrapper">
             <img src={logo} alt="logo" className="logo"/>
           </div>
           <div className="login-wrapper my-auto">
+          { errorMessage &&
+            <div className="alert alert-danger"> {errorMessage} </div> }
             <h1 className="login-title">Login</h1>
             <form action="#!">
               <div className="form-group">
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" className="form-control" placeholder="email@example.com" onChange={e => setEmail(e.target.value)} />
+                <input type="email" name="email" id="email" className="form-control" placeholder="email@example.com" onChange={e => setEmail(e.target.value)} required/>
               </div>
               <div className="form-group mb-4">
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" className="form-control" placeholder="enter your passsword" onChange={e => setPassword(e.target.value)}/>
+                <input type="password" name="password" id="password" className="form-control" placeholder="enter your passsword" onChange={e => setPassword(e.target.value)} required/>
               </div>
               <input name="login" id="login" className="btn btn-block login-btn" type="button" value="Login" onClick={handleSubmit}/>
             </form>
