215f46e9873637bc572e4a49c1c5bb91b8d77bdb
Hamado Dene <hamado.dene@studio.unibo.it>
2021-05-31 23:34:02 +0200

    Some fix

--- frontend/src/Dashboard/Admin/components/Api/UserList.js
+++ frontend/src/Dashboard/Admin/components/Api/UserList.js
@@ -30,6 +30,13 @@ class UserList extends Component {
         ],
         isOpen: false,
         modalInfo: [],
+        user:[{
+            email: '',
+            name: '',
+            role: '',
+            password: ''
+        }],
+        checkPassword: ''
     }
 
     openModal = () => this.setState({ isOpen: true });
@@ -51,10 +58,34 @@ class UserList extends Component {
             }
 
         })
-            .then(response => response.json())
-            .then(json => {
-                this.setState({users: json})
+        .then(response => response.json())
+        .then(json => {
+            this.setState({users: json})
         });
+    }
+
+    handleUserSaveChange() {
+        fetch("/api/account/" +JSON.stringify(localStorage.getItem('token')).id, {
+            method: 'PATCH',
+            withCredentials: true,
+            credentials: 'include',
+            headers: {
+                'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('token')).token ,
+            },
+            data: this.state.user
+        }).then(response => response.json())
+        .then(response =>  {
+            this.setState({users: json});
+            this.props.history.push('/admin');
+        })
+    }
+
+    handleChange = e => {
+      e.persist();
+
+      this.setState(prevState => ({
+        user: { ...prevState.user,  [e.target.name]: e.target.value }
+      }))
     }
 
   render() {
@@ -104,16 +135,34 @@ class UserList extends Component {
                    <Modal.Title>{ this.state.modalInfo.name }</Modal.Title>
                  </Modal.Header>
                  <Modal.Body>
-
+                   <form onSubmit={this.handleUserSaveChange}>
+                     <div className="modal-body">
+                       <div className="form-group">
+                         <label htmlFor="username">Username</label>
+                         <input type="text" className="form-control" id="username" name="email" defaultValue={this.state.modalInfo.email} onChange={this.handleChange} placeholder={this.state.modalInfo.email}/>
+                       </div>
+                       <div className="form-group">
+                         <label htmlFor="role">Role</label>
+                         <input type="text" className="form-control" id="role" name="role" defaultValue={this.state.modalInfo.role} onChange={this.handleChange} placeholder={this.state.modalInfo.role}/>
+                       </div>
+                      <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" className="form-control" id="name" name="name" defaultValue={this.state.modalInfo.name} onChange={this.handleChange} placeholder={this.state.modalInfo.name}/>
+                      </div>
+                       <div className="form-group">
+                         <label htmlFor="password1">Password</label>
+                         <input type="password" className="form-control" name="password" id="password1" onChange={this.handleChange}  placeholder="Password"/>
+                       </div>
+                       <div className="form-group">
+                         <label htmlFor="password2">Confirm Password</label>
+                         <input type="password" className="form-control" id="password2" onChange={e => this.setState({checkPassword: e.target.value})} placeholder="Confirm Password"/>
+                       </div>
+                     </div>
+                     <div className="modal-footer border-top-0 d-flex justify-content-center">
+                       <button type="submit" className="btn btn-success">Save Changes</button>
+                     </div>
+                   </form>
                  </Modal.Body>
-                 <Modal.Footer>
-                   <Button variant="secondary" onClick={this.closeModal}>
-                     Close
-                   </Button>
-                   <Button variant="primary" onClick={this.closeModal}>
-                     Save Changes
-                   </Button>
-                 </Modal.Footer>
                </Modal>
           </div>
         </div>
