c62d1d1be3ff59d1c2ec5a5327ed237c53587554
Hamado Dene <hamado.dene@studio.unibo.it>
2021-06-03 20:01:39 +0200

    Working about filter charts by date

--- frontend/src/Dashboard/Admin/components/Api/Statistics.js
+++ frontend/src/Dashboard/Admin/components/Api/Statistics.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import PieChart from './components/PieChart'
 import LineChart from './components/LineChart'
+import DateRangePicker from 'react-bootstrap-daterangepicker';
+import 'bootstrap-daterangepicker/daterangepicker.css';
+import moment from 'moment';
 
 class Statistics extends Component {
     state = {chartData: []}
@@ -12,24 +15,47 @@ class Statistics extends Component {
        });
     }
 
+  handleFilterChartsByDate(event, picker) {
+       console.warn("All " + this.state.chartData);
+       fetch("/api/waste?groupByType=true&includeDataPoints=true&from="+moment(picker.startDate).format('YYYY-MM-DD')+"&to="+moment(picker.endDate).format('YYYY-MM-DD'))
+       .then(response => response.json())
+       .then(json => {
+           console.log(json);
+           if(json.length > 0) {
+               this.setState({chartData: json})
+           }
+       });
+  }
+
   render() {
     return(
-        <div className="row">
-          <div className="col-xl-8 col-lg-7">
-              <div className="card shadow mb-4">
-                  <div className="card-body">
-                         <LineChart title="Waste" data={this.state.chartData}/>
-                  </div>
-              </div>
+        <div>
+          <div className="d-inline-flex p-2">
+              <DateRangePicker
+                  initialSettings={{ startDate: '01/05/2021', endDate: '01/06/2021' }}
+                  onApply={this.handleFilterChartsByDate.bind(this)}
+                >
+                  <input type="text" className="form-control" />
+              </DateRangePicker>
           </div>
 
-          <div className="col-xl-4 col-lg-5">
-              <div className="card shadow mb-4">
-                  <div className="card-body">
-                    <PieChart title="Waste" data={this.state.chartData}/>
+            <div className="row">
+              <div className="col-xl-8 col-lg-7">
+                  <div className="card shadow mb-4">
+                      <div className="card-body">
+                             <LineChart title="Waste" data={this.state.chartData}/>
+                      </div>
                   </div>
               </div>
-          </div>
+
+              <div className="col-xl-4 col-lg-5">
+                  <div className="card shadow mb-4">
+                      <div className="card-body">
+                        <PieChart title="Waste" data={this.state.chartData}/>
+                      </div>
+                  </div>
+              </div>
+            </div>
         </div>
     )
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import './Dashboard/Admin/AdminDashboard.scss';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
+import 'bootstrap-daterangepicker/daterangepicker.css';
 
 //Boostrap Libs
 import $ from 'jquery';
