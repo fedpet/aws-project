32ec90faed0116c9f7e41695e8e599204a9a3055
fedpet <federico.pettinari2@studio.unibo.it>
2021-06-06 14:00:01 +0200

    Prepare

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './css/styles.scss';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
@@ -14,8 +13,3 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
     <App />, document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
